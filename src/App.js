af999f18ddc18a6fb061ee02412038080a23bece
kimbeomjin95 <kimbeomjin95@github.com>
2021-07-29 09:55:45 +0900

    CRA 및 상태(state)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [state, setState] = useState(false); // 바뀔 여지가 있는 부분이 state(상태)
+  console.log('state: '+state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => setState(!state)}>{state ? 'Liked-true' : 'Liked-false'}</button> // 상태가 변경되면 화면이 저절로 바뀜
   );
 }
 
