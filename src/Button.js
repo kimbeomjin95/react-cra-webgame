26e061509af76b38919d13c8b0131d5daa7a3a33
kimbeomjin95 <kimbeomjin95@github.com>
2021-07-29 16:27:39 +0900

    버튼 이름 수정

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ import React, {useState} from 'react';
 const LikeBtn = () => {
   const [state, setState] = useState(false); // 바뀔 여지가 있는 부분이 state(상태)
   return (
-    <button onClick={() => setState(!state)}>{state ? '좋아요 클릭' : '좋아요 눌렀음'}</button> // 상태가 변경되면 화면이 저절로 바뀜
+    <button onClick={() => setState(!state)}>{state ? 'Like' : 'Liked'}</button> // 상태가 변경되면 화면이 저절로 바뀜
     // JSX: JS + XML
   );
 }
